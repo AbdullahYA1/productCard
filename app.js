692657aa8bd7390e63657bbdd012411eb20289be
AbdullahYA1 <abdullah.y.alowias@gmail.com>
2025-10-08 21:15:53 +0300

    create product card

---
+++ app.js
@@ -0,0 +1,9 @@
+// let coffe = {
+//   coffeType: ["saudi coffe" , "V60" , "ايس امريكانو" ,"water"],
+//   peansType: ["كولمبي","برازيلي","خولاني","يمني","اثيوبي"]
+// }
+let image = "/images/carfuture.png"
+let productName = "mim home stsyem"
+let productDes = " خل بيتك اذكى"
+let critera = ["wifi ","bluetouth" ,"z-wave"]
+let price = 1299 
