c218b395e4fc31b4fcb83d8f17600c97ed5fe130
AbdullahYA1 <abdullah.y.alowias@gmail.com>
2025-10-09 18:13:56 +0300

    commit products

--- app.js
+++ app.js
@@ -1,9 +1,44 @@
-// let coffe = {
-//   coffeType: ["saudi coffe" , "V60" , "ايس امريكانو" ,"water"],
-//   peansType: ["كولمبي","برازيلي","خولاني","يمني","اثيوبي"]
-// }
-let image = "/images/carfuture.png"
-let productName = "mim home stsyem"
-let productDes = " خل بيتك اذكى"
-let critera = ["wifi ","bluetouth" ,"z-wave"]
-let price = 1299 
+const products = [
+    {
+        image:"images/carfuture.png",
+        id:1,
+        name:"camera",
+        price: 3000
+    }, 
+    {
+        image:"images/carfuture.png",
+        id:2,
+        name:"laptop",
+        price: 5000
+    },
+    {
+        image:"images/carfuture.png",
+        id:3,
+        name:"phone",
+        price: 2000
+    },
+    {
+        image:"images/carfuture.png",
+        id:4,
+        name:"watch",
+        price: 1000
+    }
+
+]
+
+const productCard = document.getElementById("product-list")
+
+products.forEach(product => {
+    productCard.innerHTML += `
+    <div class="col">
+        <div class="card h-100">
+          <img src="${product.image}" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">${product.name}</h5>
+            <p class="card-text">Price: $${product.price}</p>
+            <button class="btn btn-primary">Add to Cart</button>
+          </div>
+        </div>
+      </div>
+    `;
+});
